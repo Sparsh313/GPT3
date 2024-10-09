80e2eecabe17de028252509a63a04677a884da5a
Sparsh Singh <sparsh312003@gmail.com>
2024-10-10 03:01:41 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Features } from "./Feature/Feature";
+export { default as Cta } from "./Cta/Cta";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Brand } from "./Brand/Brand";
